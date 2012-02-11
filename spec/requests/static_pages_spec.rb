5cdc192f7f098bf889fd84d3220ea55d3784c087
Julian Solo <juliansolo004@gmail.com>
2012-02-10 20:54:56 -0600

    default rails page removed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,11 +6,11 @@ describe "StaticPages" do
   
   describe "Home page" do
      it "should have the h1 'Sample App'" do 
-        visit '/static_pages/home'
+        visit home_path
         page.should have_selector('h1', :text => "Sample App")        
      end
      it "should have the title 'Home'" do
-       visit '/static_pages/home'
+       visit home_path
        page.should have_selector('title',
                          :text => "#{base_title}Home")
      end
@@ -18,11 +18,11 @@ describe "StaticPages" do
   
   describe "Help page" do
       it "should have the h1 'Help'" do
-        visit '/static_pages/help'
+        visit help_path
         page.should have_selector('h1', :text => 'Help')
       end
       it "should have the title 'Help'" do
-        visit '/static_pages/help'
+        visit help_path
         page.should have_selector('title',
                           :text => "#{base_title}Help")
       end
@@ -30,22 +30,22 @@ describe "StaticPages" do
   
   describe "About page" do
       it "should have the h1 'About'" do
-        visit '/static_pages/about'
+        visit about_path
         page.should have_selector('h1', :text => 'About Us')
       end
       it "should have the title 'About Us'" do
-        visit '/static_pages/about'
+        visit about_path
         page.should have_selector('title',
                       :text => "#{base_title}About Us")
       end
   end
   describe "Contact page" do
       it "should have the h1 'Contact'" do
-        visit '/static_pages/contact'
+        visit contact_path
         page.should have_selector('h1', :text => 'Contact Us')
       end
       it "should have the title 'Contact Us'" do
-        visit '/static_pages/contact'
+        visit contact_path
         page.should have_selector('title',
                       :text => "#{base_title}Contact Us")
       end
