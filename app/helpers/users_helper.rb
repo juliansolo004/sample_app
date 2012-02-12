31093e5c8bbb8ac2d3d1e84d5f5aea8e89a9f191
Julian Solo <juliansolo004@gmail.com>
2012-02-12 11:12:40 -0600

    styling flash

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://gravatar.com/avantar/#{gravatar_id}.png"
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png"
     image_tag( gravatar_url, alt: user.name, class: "gravatar")
   end
 end
